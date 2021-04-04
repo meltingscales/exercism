7e5633e02a393dbab084bc2b20c8846d0b7ef7e7
Henry Post <HenryFBP@gmail.com>
2021-04-04 16:32:33 -0500

    finish transposeCharAtbash

--- c/atbash-cipher/src/atbash_cipher.c
+++ c/atbash-cipher/src/atbash_cipher.c
@@ -4,12 +4,15 @@
 #include <string.h>
 
 static char *ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+static int MAX_ALPHA_POS = 25; //26 total letters in ALPHABET
 
 // Given a character, ensure it is lowercase.
-char ensureLowercase(char c) {
+char ensureLowercase(char c)
+{
 
   // If 'c' is uppercase A-Z
-  if (c > 'A' && c <= 'Z') {
+  if (c > 'A' && c <= 'Z')
+  {
     return c + (abs('a' - 'A'));
     // Move `c` up by the positive difference
     // between the capital and lowercase letters
@@ -18,28 +21,44 @@ char ensureLowercase(char c) {
   return c;
 }
 
-char transposeCharAtbash(char c) {
+char transposeCharAtbash(char c)
+{
   char lcc = ensureLowercase(c);
   int foundidx = -1;
 
-  for (int i = 0; i < strlen(ALPHABET); i++) {
-    if (ALPHABET[i] == c) {
-      foundidx =
-          i; // This needs to have 27 subtracted from it and then normalized to
-             // be positive, to find the character we should have.
+  for (int i = 0; i < strlen(ALPHABET); i++)
+  {
+    if (ALPHABET[i] == c)
+    {
+      // This needs to have 25 subtracted from it and then normalized to be positive, to find the character we should have.
+      foundidx = i;
       break;
     }
   }
 
-  if (foundidx == -1) {
-    fprintf(stderr,
-            "foundidx is -1! This means 'c', '%c', or %d is an invalid cipher "
-            "character.",
-            c, c);
+  if (foundidx == -1)
+  {
+    fprintf(stderr, "foundidx is -1! This means 'c', '%c', or %d is an invalid cipher character.", c, c);
     // error, this means c is invalid, panic and quit
+    exit(-1);
   }
+
+  int transposedidx = abs(foundidx - MAX_ALPHA_POS);
+  printf("transposedidx = %d\n", transposedidx);
+
+  return ALPHABET[transposedidx];
 }
 
 char *atbash_encode(const char *input) { return "lol"; }
 
 char *atbash_decode(const char *input) { return "lol"; }
+
+int main(int argc, char *argv[])
+{
+
+  char foo = transposeCharAtbash('z');
+
+  printf("%c\n", foo);
+
+  printf("lol\n");
+}
