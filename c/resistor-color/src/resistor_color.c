5e14467a6213863b81e7ef63893c3005c6ccd76f
Henry Post <HenryFBP@gmail.com>
2021-05-19 16:34:39 -0500

    finish resistor-color exercise

--- c/resistor-color/src/resistor_color.c
+++ c/resistor-color/src/resistor_color.c
@@ -1,1 +1,15 @@
 #include "resistor_color.h"
+
+resistor_band_t all_colors[10] = {BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE};
+
+
+int color_code(resistor_band_t rc)
+{
+    return (int)rc;
+}
+
+resistor_band_t *colors()
+{
+
+    return all_colors;
+}
--- c/resistor-color/src/resistor_color.h
+++ c/resistor-color/src/resistor_color.h
@@ -1,8 +1,22 @@
 #ifndef RESISTOR_COLOR_H
 #define RESISTOR_COLOR_H
+#endif
 
-typedef enum {
-
+typedef enum
+{
+    BLACK = 0,
+    BROWN = 1,
+    RED = 2,
+    ORANGE = 3,
+    YELLOW = 4,
+    GREEN = 5,
+    BLUE = 6,
+    VIOLET = 7,
+    GREY = 8,
+    WHITE = 9,
 } resistor_band_t;
 
-#endif
+resistor_band_t *colors();
+
+int color_code(resistor_band_t rc);
+
