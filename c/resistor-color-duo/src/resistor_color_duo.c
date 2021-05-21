8b64023b9790bd90cefde0682f0ceb3b6a02ff5d
Henry Post <HenryFBP@gmail.com>
2021-05-21 08:32:55 -0500

    finisish resistor_color_duo

--- c/resistor-color-duo/src/resistor_color_duo.c
+++ c/resistor-color-duo/src/resistor_color_duo.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+
 #include "resistor_color_duo.h"
+
+int color_code(resistor_band_t *rc)
+{
+    return (rc[0] * 10) + (rc[1] * 1);
+}
--- c/resistor-color-duo/src/resistor_color_duo.h
+++ c/resistor-color-duo/src/resistor_color_duo.h
@@ -2,3 +2,21 @@
 #define RESISTOR_COLOR_DUO_H
 
 #endif
+
+typedef enum
+{
+    BLACK = 0,
+    BROWN = 1,
+    RED = 2,
+    ORANGE = 3,
+    YELLOW = 4,
+    GREEN = 5,
+    BLUE = 6,
+    VIOLET = 7,
+    GREY = 8,
+    WHITE = 9,
+} resistor_band_t;
+
+resistor_band_t *colors();
+
+int color_code(resistor_band_t *rc);
