d6590b2342ceb72357ce38b3a566c1d970538df0
Henry Post <HenryFBP@gmail.com>
2021-05-22 20:10:51 -0500

    start working on init allergen struct

--- c/allergies/src/allergies.c
+++ c/allergies/src/allergies.c
@@ -1,1 +1,20 @@
 #include "allergies.h"
+#include <stdlib.h>
+
+int is_allergic_to()
+{
+    return -1;
+}
+
+allergen_list_t get_allergens(int bitfield)
+{
+    struct allergen_list_t allergen_list = malloc(
+        sizeof(allergen_list_t));
+    // malloc for allergen_list_t. 
+    // `sizeof` knows how large allergen_list_t is because allergen_list_t.allergens is a static length.
+    // If it were not defined statically, we would need to add `sizeof` calls together.
+
+    printf("allergen_list_t is %ul bytes large...",sizeof(allergen_list_t));
+
+    return allergen_list;
+}
